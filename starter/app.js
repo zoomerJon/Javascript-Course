6af6a33746fe8ae7ff1d3eaee494e2a0f59e6d9d
zoomerJon <jovalle@nevada.unr.edu>
2020-10-15 17:17:49 -0700

    Test

--- starter/app.js
+++ starter/app.js
@@ -15,7 +15,7 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random()*6) + 1;
+dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
